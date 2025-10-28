d240c0d1d5d645fae7ea1e7b75f2de69a6486f91
AakashPrakash <aakashprakash27@gmail.com>
2025-10-28 23:59:53 +0530

    Person program 1a

---
+++ Person.java
@@ -0,0 +1,18 @@
+public class Person {
+    private String name;
+    private int age=18;
+
+    public Person(String name, int age){
+        this.name=name;
+        this.age=age;
+    }
+
+    public void display(){
+        System.out.println("My Name is : "+this.name+" .I am "+this.age+" years old.");
+    }
+
+    public static void main(String[] args) {
+        Person aakash = new Person("aakash",25);
+        aakash.display();
+    }
+}
